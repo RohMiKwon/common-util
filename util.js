444c1519ce036bd4ccbb19eab77dfbd399184c0c
RohMiKwon <rohmi@hivelab.co.kr>
2016-08-03 11:56:09 +0900

    feature: 터치 디바이스 체크 기능 추가
    isTouchDevices

---
+++ util.js
@@ -0,0 +1,15 @@
+/**
+* @namespace
+*/
+var rmk = rmk || {};
+rmk.util = rmk.uitl || {};
+
+/**
+* @description 공통 기능
+*/
+var commonUtils = rmk.util = {
+	isTouchDevice: function(){
+		return 'ontouchstart' in window 	// works on most browsers
+		|| navigator.maxTouchPoints;		// works on IE10/11 and Surface
+	}
+};
