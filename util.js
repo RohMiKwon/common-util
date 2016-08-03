237521d43caf152660e10f7c010401cba7d7cc5f
RohMiKwon <rohmi@hivelab.co.kr>
2016-08-03 14:16:49 +0900

    feature: 스크린 사이즈 구하는 기능 추가
    getScreenSize
    브라우저 호환성을 위한 구문이 포함되어 있음

--- util.js
+++ util.js
@@ -11,5 +11,12 @@ var commonUtils = rmk.util = {
 	isTouchDevice: function(){
 		return 'ontouchstart' in window 	// works on most browsers
 		|| navigator.maxTouchPoints;		// works on IE10/11 and Surface
+	},
+	
+	getScreenSize: function(){
+		return {
+			width: window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth,
+			height: window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+		}
 	}
 };
